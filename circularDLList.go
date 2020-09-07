b9eb62aeaa2940a1962c1be10790260a009f645d
userr2232 <reynaldo.rz.26@gmail.com>
2020-09-07 01:29:36 -0500

    refactor

--- circularDLList.go
+++ circularDLList.go
@@ -1,7 +1,5 @@
 package main
 
-import "fmt"
-
 type Node struct {
 	Key string
 	Value int
@@ -15,6 +13,14 @@ type CDLL struct {
 	Head *Node
 }
 
+func (l *CDLL) placeFront(cur *Node) {
+	cur.Next = l.Head
+	cur.Prev = l.Head.Prev
+	l.Head.Prev.Next = cur
+	l.Head.Prev = cur
+	l.Head = cur
+}
+
 func (l *CDLL) AddAtHead(k string, v int) *Node {
 	l.Len++
 	cur := &Node{k, v, nil, nil}
@@ -24,31 +30,12 @@ func (l *CDLL) AddAtHead(k string, v int) *Node {
 		cur.Prev = cur
 		return cur
 	}
-	cur.Next = l.Head
-	cur.Prev = l.Head.Prev
-	l.Head.Prev.Next = cur
-	l.Head.Prev = cur
-	l.Head = cur
+	l.placeFront(cur)
 	return cur
 }
 
 func (l *CDLL) UpdateHead(cur *Node) {
 	cur.Prev.Next = cur.Next
 	cur.Next.Prev = cur.Prev
-	cur.Next = l.Head
-	cur.Prev = l.Head.Prev
-	l.Head.Prev.Next = cur
-	l.Head.Prev = cur
-	l.Head = cur
-}
-
-func (l *CDLL) PrintList() {
-	fmt.Println("printing list with length ", l.Len)
-	fmt.Printf("%s: %d\n", l.Head.Key, l.Head.Value)
-	cur := l.Head.Next
-	for cur != l.Head {
-		fmt.Printf("%s: %d\n", cur.Key, cur.Value)
-		cur = cur.Next
-	}
-	fmt.Println("done")
+	l.placeFront(cur)
 }
